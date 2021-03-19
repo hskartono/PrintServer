1d6fd03407312f16df60cfed1f9eaf894df8814d
Harry Saputra Kartono <hskartono@gmail.com>
2021-03-19 21:31:33 +0700

    Add project files.

---
+++ PrintServer/Controllers/PrintController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintServer.Controllers
+{
+	[Route("api/v4/[controller]")]
+	[ApiController]
+	public class PrintController : ControllerBase
+	{
+		[HttpPost]
+		public async Task<IActionResult> Print(printRequest request)
+		{
+			if(request.request.UserName != "test")
+			{
+				ModelState.AddModelError("UserName", "Invalid username");
+				return ValidationProblem();
+			}
+
+			var b64Str = request.request.Document;
+			var path = System.IO.Path.Combine(AppContext.BaseDirectory, "Result", request.request.FileName);
+			Byte[] bytes = Convert.FromBase64String(b64Str);
+			System.IO.File.WriteAllBytes(path, bytes);
+
+			return Ok();
+		}
+	}
+}
---
+++ PrintServer/Result/readme.txt
@@ -0,0 +1,1 @@
+﻿lokasi file upload
